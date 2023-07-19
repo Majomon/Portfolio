c016e67a566c03a3230c7acb0008b3948387cf94
Majomon <mauri.monzon91@gmail.com>
2023-07-19 11:59:18 -0300

    Mejorado skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,20 +13,40 @@ import Git from "../../assets/skills/GIT.png";
 import Bootstrap from "../../assets/skills/Bootstrap.png";
 import Tailwind from "../../assets/skills/Tailwind.png";
 import Sass from "../../assets/skills/Sass.png";
+import Sequelize from "../../assets/skills/Sequelize.png";
+import Firebase from "../../assets/skills/Firebase.png";
 
 const techsIcons = [
-  { name: "HTML", img: HTML },
-  { name: "CSS", img: CSS },
-  { name: "Javascript", img: Js },
-  { name: "React", img: React },
-  { name: "React Native", img: ReactNative },
-  { name: "Postgress", img: Postgress },
-  { name: "Redux", img: Redux },
-  { name: "Node", img: Node },
-  { name: "Git", img: Git },
-  { name: "Bootstrap", img: Bootstrap },
-  { name: "Tailwind", img: Tailwind },
-  { name: "Sass", img: Sass },
+  {
+    name: "HTML",
+    img: HTML,
+    url: "https://developer.mozilla.org/es/docs/Web/HTML",
+  },
+  {
+    name: "CSS",
+    img: CSS,
+    url: "https://developer.mozilla.org/es/docs/Web/CSS",
+  },
+  {
+    name: "Javascript",
+    img: Js,
+    url: "https://developer.mozilla.org/es/docs/Web/JavaScript",
+  },
+  { name: "React", img: React, url: "https://es.react.dev/" },
+  { name: "React Native", img: ReactNative, url: "https://reactnative.dev/" },
+  { name: "Postgress", img: Postgress, url: "https://www.postgresql.org/" },
+  { name: "Redux", img: Redux, url: "https://redux.js.org/" },
+  { name: "Node", img: Node, url: "https://nodejs.org/es" },
+  { name: "Git", img: Git, url: "https://git-scm.com/" },
+  { name: "Bootstrap", img: Bootstrap, url: "https://getbootstrap.com/" },
+  { name: "Tailwind", img: Tailwind, url: "https://tailwindcss.com/" },
+  { name: "Sass", img: Sass, url: "https://sass-lang.com/" },
+  { name: "Sequelize", img: Sequelize, url: "https://sequelize.org/" },
+  {
+    name: "Firebase",
+    img: Firebase,
+    url: "https://firebase.google.com/?hl=es",
+  },
 ];
 
 const Skills = () => {
@@ -42,14 +62,18 @@ const Skills = () => {
       </div>
       <div className="w-10/12 mx-auto grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
         {techsIcons.map((icon, index) => (
-          <motion.div
-            key={index}
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", stiffness: 1000 }}
-            className="w-28 h-28 flex items-center justify-center p-3 bg-black dark:bg-white rounded-full cursor-pointer hover:shadow-[0_0_12px_#000] dark:hover:shadow-[0_0_12px_#fff]"
-          >
-            <motion.img src={icon.img} alt={icon.name} className="w-20" />
-          </motion.div>
+          <div key={index}>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 1000 }}
+              className="w-28 h-28 flex items-center justify-center p-3 bg-black dark:bg-white rounded-full cursor-pointer hover:shadow-[0_0_12px_#000] dark:hover:shadow-[0_0_12px_#fff]"
+            >
+              <a href={icon.url} target="_blank" rel="noopener noreferrer">
+                <motion.img src={icon.img} alt={icon.name} className="w-20" />
+              </a>
+            </motion.div>
+            <h4 className="text-center pt-2">{icon.name}</h4>
+          </div>
         ))}
       </div>
     </div>
